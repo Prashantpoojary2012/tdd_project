30a8a0104f54f72b64d5248b89c4f77bff6478f0
Prashant <prashant@myshopygram.com>
2025-01-19 19:57:43 +0530

    Adding the postive condition in testing

--- findFirstDup.js
+++ findFirstDup.js
@@ -1,5 +1,15 @@
-function findFirstDup(){
-    return 'First Number Value'
+// Adding the function for finding the first number duplicate in array that return index and value
+function findFirstDup(arr) {
+    const seen = new Map();
+    for(let i=0;i<arr.length;i++) {
+        const num = arr[i];
+        if(seen.has(num)) {
+            return {index: i, value: num}
+        }
+        seen.set(num,i);
+    }
+    return { index: -1, value: null}
 }
+
 
 module.exports = findFirstDup;
--- findFirstDup.test.js
+++ findFirstDup.test.js
@@ -1,1 +1,9 @@
 const findFirstDup = require('./findFirstDup');
+
+describe('findFirstDup',() =>{
+    it('should return the index and value of the first duplicate in an array',() =>{
+        const input = [1,2,3,4,5,2,7,9,3,0,1];
+        const result = findFirstDup(input);
+        expect(result).toEqual({index: 5, value: 2});
+    })
+});
